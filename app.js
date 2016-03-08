65f8ed7224ba729d82df19d36dbd72efe7ac2192
Jarmo Habakuk <jarmhab@greeny.cs.tlu.ee>
2016-03-08 22:27:39 +0200

    viimati lisatud nupud

--- app.js
+++ app.js
@@ -10,12 +10,16 @@
 
   //  this.ScoreBoard = document.getElementById('ScoreBoard');
     this.routes = ScoreBoard.routes;
+    this.createdTeam = null;
+    this.createdScore = null;
 
     console.log(this);
 
     //panen rakenduse tööle
     this.init();
   };
+
+  window.ScoreBoard = ScoreBoard;
 
   ScoreBoard.routes = {
     "home-view": {
@@ -60,18 +64,69 @@
     },
     bindMouseEvents: function(){
       document.querySelector('.add-teams').addEventListener('click', this.addNewClick.bind(this));
+      document.querySelector('.add-home').addEventListener('click', this.update.bind(this));
+      document.querySelector('.add-away').addEventListener('click', this.update.bind(this));
+      document.querySelector('.add-home-2').addEventListener('click', this.update2.bind(this));
+      document.querySelector('.add-away-2').addEventListener('click', this.update2.bind(this));
+      document.querySelector('.add-home-3').addEventListener('click', this.update3.bind(this));
+      document.querySelector('.add-away-3').addEventListener('click', this.update3.bind(this));
+    },
+    update: function(event){
+      console.log(event.target.dataset);
+
+      console.log(event.target.dataset.name);
+
+      if(event.target.dataset.home){
+          this.createdTeam.updateScore('home');
+      }else if(event.target.dataset.away){
+          this.createdTeam.updateScore('away');
+      }
+
+    },
+    update2: function(event){
+
+      if(event.target.dataset.home){
+          this.createdTeam.updateScore2('home');
+      }else if(event.target.dataset.away){
+          this.createdTeam.updateScore2('away');
+      }
+
+    },
+    update3: function(event){
+
+      if(event.target.dataset.home){
+          this.createdTeam.updateScore3('home');
+      }else if(event.target.dataset.away){
+          this.createdTeam.updateScore3('away');
+      }
+
     },
     addNewClick: function(event){
       // lisa uus purk
       var home_team = document.querySelector('.home_team').value;
       var away_team = document.querySelector('.away_team').value;
+      var home_team_score = document.querySelector('.home_team_score').value;
+      var away_team_score = document.querySelector('.away_team_score').value;
       console.log(home_team + '<------>' + away_team);
-      if (home_team !== "" && away_team !== ""){
+      console.log(home_team_score + '<------>' + away_team_score);
+      if (home_team !== "" && away_team !== "" && home_team_score !== "" && away_team_score !== ""){
 
-        var new_teams = new Teams(home_team, away_team);
-    //    var li = new_jar.createHtmlElement();
-    //    document.querySelector('.list-of-jars').appendChild(li);
+        var new_teams = new Teams(home_team, away_team, home_team_score, away_team_score);
+        //var new_scores = new Scores(home_team_score, away_team_score);
+
+        this.createdTeam = new_teams;
+        console.log(this.createdTeam);
+      //  this.createdScore = new_scores;
+
+        var div = new_teams.generateDiv();
+      //  var div2 = new_scores.generateDiv();
+
+        document.querySelector(".names").appendChild(div);
+      //  document.querySelector(".scores").appendChild(div2);
+
+
         document.querySelector("span.home_team-error").innerHTML="";
+        document.querySelector("span.away_team-error").innerHTML="";
       } else {
 
         //mis oli tühi
@@ -123,6 +178,101 @@
 
 
   };
+
+  var Teams = function(a, b, c, d){
+    this.home = a;
+    this.away = b;
+    this.home_score = c;
+    this.away_score = d;
+    this.div = null;
+  };
+
+  // var Scores = function(a, b){
+  //   this.home = a;
+  //   this.away = b;
+  //   this.div = null;
+  // };
+
+  Teams.prototype = {
+    generateDiv: function(){
+      var div = document.createElement('div');
+      div.innerHTML = this.home+" - "+this.away +'<br>';
+      div.innerHTML += this.home_score+" - "+this.away_score;
+      // a = 5
+      // a += 10 // a = 15 // a = a + 10
+      this.div = div;
+      console.log(div);
+      return div;
+    },
+    updateScore: function(home_or_away){
+      if(home_or_away == "home"){
+        this.home_score++;
+      //  this.div.innerHTML = this.home+" - "+this.away;
+        this.div.innerHTML = this.home+" - "+this.away +'<br>';
+        this.div.innerHTML += this.home_score+" - "+this.away_score;
+      }
+
+      if(home_or_away == "away"){
+        this.away_score++;
+      //  this.div.innerHTML = this.home+" - "+this.away;
+        this.div.innerHTML = this.home+" - "+this.away +'<br>';
+        this.div.innerHTML += this.home_score+" - "+this.away_score;
+      }
+    },
+    updateScore2: function(home_or_away){
+      if(home_or_away == "home"){
+        this.home_score+=2;
+      //  this.div.innerHTML = this.home+" - "+this.away;
+        this.div.innerHTML = this.home+" - "+this.away +'<br>';
+        this.div.innerHTML += this.home_score+" - "+this.away_score;
+      }
+
+      if(home_or_away == "away"){
+        this.away_score+=2;
+      //  this.div.innerHTML = this.home+" - "+this.away;
+        this.div.innerHTML = this.home+" - "+this.away +'<br>';
+        this.div.innerHTML += this.home_score+" - "+this.away_score;
+      }
+    },
+    updateScore3: function(home_or_away){
+      if(home_or_away == "home"){
+        this.home_score+=3;
+      //  this.div.innerHTML = this.home+" - "+this.away;
+        this.div.innerHTML = this.home+" - "+this.away +'<br>';
+        this.div.innerHTML += this.home_score+" - "+this.away_score;
+      }
+
+      if(home_or_away == "away"){
+        this.away_score+=3;
+      //  this.div.innerHTML = this.home+" - "+this.away;
+        this.div.innerHTML = this.home+" - "+this.away +'<br>';
+        this.div.innerHTML += this.home_score+" - "+this.away_score;
+      }
+    },
+
+  };
+
+  // Scores.prototype = {
+  //   generateDiv: function(){
+  //     var div = document.createElement('div');
+  //     div.innerHTML = this.home+" - "+this.away;
+  //     this.div = div;
+  //     console.log(div);
+  //     return div;
+  //   },
+    // updateScore: function(home_or_away){
+    //   if(home_or_away == "home"){
+    //     this.home++;
+    //     this.div.innerHTML = this.home+" - "+this.away;
+    //   }if(home_or_away == "away"){
+    //     this.away++;
+    //     this.div.innerHTML = this.home+" - "+this.away;
+    //   }
+    // }
+//  };
+
+  window.Teams = Teams;
+//  window.Scores = Scores;
 
     window.onload = function(){
       var app = new ScoreBoard();
